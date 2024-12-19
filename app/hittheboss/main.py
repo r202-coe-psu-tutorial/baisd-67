a491db68a5025f169bc7c342f4ffb204b0a76cfa
Thanathip Limna <boatkrap@gmail.com>
2024-12-19 09:28:38 +0700

    move monster

--- app/hittheboss/main.py
+++ app/hittheboss/main.py
@@ -1,9 +1,12 @@
+from random import randint
+
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget  
 
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
 
 class Monster(Widget):
     velocity_x = NumericProperty(0)
@@ -19,11 +22,30 @@ class Monster(Widget):
         self.pos = Vector(*self.velocity) + self.pos
 
 class HitTheBossGame(Widget):
-    pass 
+    monster = ObjectProperty(None)
+
+
+    def release_monster(self):
+        self.monster.center = self.center
+        self.monster.velocity = Vector(4, 0).rotate(randint(0, 360))
+
+    def update(self, dt):
+        self.monster.move()
+
+        # bounce off top and bottom
+        if (self.monster.y < 0) or (self.monster.top > self.height):
+            self.monster.velocity_y *= -1
+
+        # bounce off left and right
+        if (self.monster.x < 0) or (self.monster.right > self.width):
+            self.monster.velocity_x *= -1
 
 class HitTheBossApp(App):
     def build(self):
-        return HitTheBossGame()
+        game = HitTheBossGame()
+        game.release_monster()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
 
 if __name__ == '__main__':
     HitTheBossApp().run()
